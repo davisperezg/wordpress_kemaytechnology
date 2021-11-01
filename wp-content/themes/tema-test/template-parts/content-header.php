9539e683eeb2fb5bb1faecacee571ae6fb5f5097
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2021-11-01 13:36:58 -0500

    color al header

--- wp-content/themes/tema-test/template-parts/content-header.php
+++ wp-content/themes/tema-test/template-parts/content-header.php
@@ -1,9 +1,9 @@
-<header class="encabezado">
+<header class="encabezado" style="background-color: red;">
     <div class="container-fluid gx-5 py-3">
         <div class="row">
             <div class="encabezado__hamburguesa col-2">
                 <a href="#">
-                    <img src="<?php echo get_template_directory_uri()?>/assets/img/icon-menu.svg" alt="menu icon">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/icon-menu.svg" alt="menu icon">
                 </a>
             </div>
             <div class="enbezado__logo col-5 col-md-2 px-2 px-md-4">
@@ -11,26 +11,26 @@
             </div>
             <div class="encabezado__menu col-6 col-md-4">
                 <?php wp_nav_menu(
-                        array(
-                            "menu" => 'menu-principal'
-                        )
-                    ); ?>
+                    array(
+                        "menu" => 'menu-principal'
+                    )
+                ); ?>
             </div>
             <div class="encabezado__cart col-5 col-md-2">
-                <a href="<?php echo home_url("sign-in");?>" class="encabezado_sign-in">
+                <a href="<?php echo home_url("sign-in"); ?>" class="encabezado_sign-in">
                     <?php do_action("plz_signin"); ?>
                 </a>
                 <a href="#" class="encabezado__link active">
-                    <img src="<?php echo get_template_directory_uri();?>/assets/img/header-empty-card.svg" alt="cart">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/header-empty-card.svg" alt="cart">
                 </a>
             </div>
         </div>
         <div class="encabezado__menu-responsive">
             <?php wp_nav_menu(
-                        array(
-                            "menu" => 'menu-responsive'
-                        )
-                    ); ?>
+                array(
+                    "menu" => 'menu-responsive'
+                )
+            ); ?>
         </div>
     </div>
 </header>
